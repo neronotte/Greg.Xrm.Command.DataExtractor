db3b5ec795481cc0d4b2f4d0cbaf9b1f31452dc0
_neronotte <riccardo.gregori@outlook.com>
2024-04-03 10:32:43 +0200

    Add project files.

---
+++ Greg.Xrm.Command.DataExtractor/DataTreeBuildCommand.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Greg.Xrm.Command.DataExtractor
+{
+	[Command("data", "tree", "build", HelpText ="")]
+	public class DataTreeBuildCommand
+	{
+		[Option("solution", "s", HelpText = "The name of the solution containing the entities to export.")]
+		[Required]
+		public string? SolutionName { get; set; }
+	}
+}
---
+++ Greg.Xrm.Command.DataExtractor/DataTreeBuildCommandExecutor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greg.Xrm.Command.DataExtractor
+{
+	public class DataTreeBuildCommandExecutor : ICommandExecutor<DataTreeBuildCommand>
+	{
+		public Task<CommandResult> ExecuteAsync(DataTreeBuildCommand command, CancellationToken cancellationToken)
+		{
+		}
+	}
+}
